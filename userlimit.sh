439ef7aa75420d12042279b90d884bbe6cc4f2c9
saeidpour80 <136351037+saeidpour80@users.noreply.github.com>
2023-08-12 21:00:46 +0330

    Update userlimit.sh

--- userlimit.sh
+++ userlimit.sh
@@ -1,14 +1,2 @@
-echo '
-#! /bin/bash
-
-limit=1
-c=$(pgrep -xcu "$PAM_USER" sshd)
-if [[ "$PAM_USER" != "root" ]] && [[ $c -ge $limit ]]
-then
-    pkill -u "$PAM_USER"
-    exit 1
-else
-    exit 0
-fi
-' > /etc/userlimit
+echo '12345' > /etc/userlimit
 
