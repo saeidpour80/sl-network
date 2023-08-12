d57fbe0ab416c340b51b58c393d7cb085e02d9d4
saeidpour80 <136351037+saeidpour80@users.noreply.github.com>
2023-08-13 00:03:02 +0330

    Add files via upload

---
+++ userlimit.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+echo '#! /bin/bash
+
+limit=1
+c=$(pgrep -xcu "$PAM_USER" sshd)
+if [[ "$PAM_USER" != "root" ]] && [[ $c -ge $limit ]]
+then
+    pkill -u "$PAM_USER"
+    exit 1
+else
+    exit 0
+fi
+' > /etc/userlimit
+
+{ chmod -v +x /etc/userlimit; } &> /dev/null
+
+echo '\n' >> /etc/pam.d/sshd
+
+if  grep -q "account    required     pam_exec.so /etc/userlimit" "/etc/pam.d/sshd" ; then
+
+else
+    echo 'account    required     pam_exec.so /etc/userlimit' >> /etc/pam.d/sshd
+fi
+
+if  grep -q "auth       required     pam_exec.so /etc/userlimit" "/etc/pam.d/sshd" ; then
+
+else
+    echo 'auth       required     pam_exec.so /etc/userlimit' >> /etc/pam.d/sshd
+fi
+
+
