d2393752a7dd611194633f05768fef91429d082f
saeidpour80 <136351037+saeidpour80@users.noreply.github.com>
2023-08-14 12:33:44 +0330

    Update userlimit.sh

--- userlimit.sh
+++ userlimit.sh
@@ -20,8 +20,12 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 printf "\n"
-echo -e "1) \033[4;31mUser connection limit\e[0m"
+echo -e "1) ${RED}User connection limit${NC}"
 echo -e "2) User connection limit + User expiration date"
 echo -e "3) User connection limit + Set expiration date after first connection"
 echo -e "4) User connection limit + User expiration date + Set expiration date after first connection"
