dee2b4580413e4fe0d709810937bd2b800fee218
Soheil Saeidpour <136351037+saeidpour80@users.noreply.github.com>
2024-01-24 14:26:04 +0330

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,11 +1,33 @@
 #! /bin/bash
 
+Color_Off='\033[0m'       # Text Reset
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+timedatectl set-timezone Asia/Tehran
+apt update -y
+apt upgrade -y
+apt install nano -y
 apt install expect -y
+
+echo "7555" | bash <(curl -Ls https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/fix-call.sh --ipv4)
 
 clear
 
 printf "IP : "
 read fsip
+while [[ -z "$fsip" ]] || [[ !("$fsip" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$) ]]
+do
+    echo "Invalid value, Try again"
+    printf "IP : "
+    read fsip
+done
 printf "Port : "
 read fsport
 while [[ -z "$fsport" ]] || [[ !("$fsport" =~ ^[0-9]+$) ]]
@@ -16,6 +38,12 @@ do
 done
 printf "Password : "
 read fspass
+while [[ -z "$fspass" ]]
+do
+    echo "Invalid value, Try again"
+    printf "Password : "
+    read fspass
+done
 
 echo ""
 
@@ -51,19 +79,41 @@ getbackup.sh
 sleep 1s
 rm /bin/getbackup.sh
 
-getpasswd=$(awk 'p; /videocall/{p=1}' /root/restore/passwd)
-echo "$getpasswd" >> /etc/passwd
-getgroup=$(awk 'p; /videocall/{p=1}' /root/restore/group)
-echo "$getgroup" >> /etc/group
-getshadow=$(awk 'p; /videocall/{p=1}' /root/restore/shadow)
-echo "$getshadow" >> /etc/shadow
-getgshadow=$(awk 'p; /videocall/{p=1}' /root/restore/gshadow)
-echo "$getgshadow" >> /etc/gshadow
-port=$(grep -oE 'Port [0-9]+' /root/restore/sshd_config | cut -d' ' -f2)
-sed -i "s/#Port 22/Port $port/" /etc/ssh/sshd_config
-sleep 1s
-rm -rf /root/restore
+check=0
+if [[ -f /root/restore/passwd ]] && [[ -f /root/restore/group ]] && [[ -f /root/restore/shadow ]] && [[ -f /root/restore/gshadow ]] && [[ -f /root/restore/sshd_config ]]
+then
+    check=1
+fi
 
-sleep 1s
-
-reboot
+if [[ $check -eq 1 ]]
+then
+    getpasswd=$(awk 'p; /videocall/{p=1}' /root/restore/passwd)
+    echo "$getpasswd" >> /etc/passwd
+    getgroup=$(awk 'p; /videocall/{p=1}' /root/restore/group)
+    echo "$getgroup" >> /etc/group
+    getshadow=$(awk 'p; /videocall/{p=1}' /root/restore/shadow)
+    echo "$getshadow" >> /etc/shadow
+    getgshadow=$(awk 'p; /videocall/{p=1}' /root/restore/gshadow)
+    echo "$getgshadow" >> /etc/gshadow
+    port=$(grep -oE 'Port [0-9]+' /root/restore/sshd_config | cut -d' ' -f2)
+    sed -i "s/#Port 22/Port $port/" /etc/ssh/sshd_config
+    sleep 1s
+    rm -rf /root/restore
+    echo -e "\n${Green}Successful${Color_Off}\n"
+    printf "Do you agree with rebooting the server ? (y/n): "
+    read ra
+    while [[ -z "$ra" ]] || [[ !("$ra" =~ ^[nNyY]+$) ]] || [[ ${#ra} -gt 1 ]]
+    do
+        echo "Invalid value, Try again"
+        printf "Do you agree with rebooting the server ? (y/n): "
+        read ra
+    done
+    lowerrs=$(echo "$ra" | tr '[:upper:]' '[:lower:]')
+    if [[ "$lowerrs" == "y" ]]
+    then
+        sleep 2s
+        reboot
+    fi
+else
+    echo -e "\n${Red}Error${Color_Off}\n"
+fi
