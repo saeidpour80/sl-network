2ae444af8a2ba1eb4fa450e6819d851406170a42
Soheil Saeidpour <136351037+saeidpour80@users.noreply.github.com>
2024-10-01 09:39:53 +0330

    Update check_snapchat.sh

--- check_snapchat.sh
+++ check_snapchat.sh
@@ -3,6 +3,11 @@
 # Snapchat domain addresses for testing
 domains=("app.snapchat.com" "feelinsonice-hrd.appspot.com" "app-analytics.snapchat.com")
 
+# ANSI color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color (reset color)
+
 # Checking ping status
 echo "Checking connectivity to Snapchat servers..."
 for domain in "${domains[@]}"
@@ -11,9 +16,9 @@ do
     ping -c 4 $domain &> /dev/null
     
     if [ $? -eq 0 ]; then
-        echo "Ping to $domain was successful."
+        echo -e "${GREEN}Ping to $domain was successful.${NC}"
     else
-        echo "Ping to $domain failed. Your IP might be blocked."
+        echo -e "${RED}Ping to $domain failed. Your IP might be blocked.${NC}"
     fi
 done
 
@@ -25,8 +30,8 @@ do
     response=$(curl -s -o /dev/null -w "%{http_code}" https://$domain)
     
     if [ $response -eq 200 ]; then
-        echo "HTTP request to $domain was successful (Status: 200)."
+        echo -e "${GREEN}HTTP request to $domain was successful (Status: 200).${NC}"
     else
-        echo "HTTP request to $domain failed (HTTP status code: $response)."
+        echo -e "${RED}HTTP request to $domain failed (HTTP status code: $response).${NC}"
     fi
 done
